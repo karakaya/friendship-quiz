95345758ac3079f8efea9cd0397bc71f9ec027a9
mkarakaya <40628802+karakaya@users.noreply.github.com>
2022-09-10 15:50:00 +0300

    add: routes

--- pkg/route/route.go
+++ pkg/route/route.go
@@ -4,7 +4,17 @@ import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(g *gin.RouterGroup) {
 	res := resource{}
+	g.GET("/{id}", res.get)
+	g.POST("/", res.create)
 
 }
 
 type resource struct{}
+
+func (r resource) create(g *gin.Context) {
+	g.JSON(200, "")
+}
+
+func (r resource) get(g *gin.Context) {
+	g.JSON(200, "")
+}
